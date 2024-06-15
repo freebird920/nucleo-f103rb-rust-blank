8c2377ce8860e04ac51ef1082ecb13cce59dfccb
freebird920 <freebird_han@naver.com>
2024-06-15 14:26:31 +0900

    refactor: Update RCC module to include ADC prescaler configuration

--- src/main.rs
+++ src/main.rs
@@ -3,202 +3,112 @@
 #![allow(unused_parens)]
 
 use core::sync::atomic::{AtomicBool, AtomicU32, Ordering};
+use cortex_m::asm::delay;
 use cortex_m_rt::{entry, exception};
-// use cortex_m::interrupt::{Mutex};
 use panic_halt as _;
-use peripherals::{
-    afio::{EXTIx_Px, AFIO}, exti::exti, gpio::{GpioXBase, Gpio}, i2c::{I2C, I2C_BASE, PCF8574_LCD}, nvic::{NVIC, NVIC_BASE}, rcc::{rcc, IOPxEN, TIMxEN}
-};
+
+use crate::peripherals::rcc::rcc;
 use rtt_target::{rprintln, rtt_init_print};
-use utils::delay::delay_sys_clk_ms;
 
 mod peripherals;
 mod utils;
 
 // const
-const PCF8574_ADDRESS: u8 = 0b100111;
-const EXTI_BASE: u32 = 0x4001_0400;
-const AFIO_BASE: u32 = 0x4001_0000;
+// const PCF8574_ADDRESS: u8 = 0b100111;
+// const EXTI_BASE: u32 = 0x4001_0400;
+// const AFIO_BASE: u32 = 0x4001_0000;
 
 // static
-static COUNT: AtomicU32 = AtomicU32::new(0);
-static REFRESH_LCD: AtomicBool = AtomicBool::new(true);
-
+// static COUNT: AtomicU32 = AtomicU32::new(0);
+// static REFRESH_LCD: AtomicBool = AtomicBool::new(true);
+static sys_clock: AtomicU32 = AtomicU32::new(0);
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let rcc = rcc::new();
-    let gpio_a = Gpio::new(GpioXBase::A);
-    let gpio_b = Gpio::new(GpioXBase::B);
-    let gpio_c = Gpio::new(GpioXBase::C);
-    let afio = AFIO::new(AFIO_BASE);
-    let exti = exti::new(EXTI_BASE);
-    let nvic: NVIC = NVIC::new(NVIC_BASE);
-    // Enable GPIOA, GPIOB and GPIOC clocks
-    rcc.cr_hsion();
-    // rcc.set_sys_clock_32MHz();
-    rcc.set_sys_clock_64MHz();
-
-    let cr_val = rcc.read_cr();
-    rprintln!("CR: {}", cr_val);
-    let cfgr_val = rcc.read_cfgr();
-    rprintln!("CFGR: {}", cfgr_val);
-
-    rcc.cfgr_adcpre(11); // ADC prescaler 8
-
-    rcc.APB2ENR_ADC1EN(true);
-
-
-    rcc.APB1ENR_TIMxEN(TIMxEN::TIM2EN, true);
-    rcc.ABP1ENR_I2C2EN(true);
-    rcc.APB2ENR_IOPx_EN(IOPxEN::IOPAEN, true);
-    rcc.APB2ENR_IOPx_EN(IOPxEN::IOPBEN, true);
-    rcc.APB2ENR_IOPx_EN(IOPxEN::IOPCEN, true);
-
-    rcc.ABP2ENR_AFIOEN(true);
-
-    gpio_a.crl_port_config(5, 0b0001); // Configure GPIOA pin 5 as output push-pull
-    gpio_b.crh_port_config(10, 0b1001); // Configure GPIOC pin 10 as output open-drain
-    gpio_b.crh_port_config(11, 0b1010); // Configure GPIOC pin 11 as output open-drain
-
-    gpio_c.crh_port_config(13, 0b0100); // PC13 is input mode
-
-    let sysclk = get_sys_clock();
-    rprintln!("System clock: {} Hz", sysclk);
-   
-    // PC0 ADC12_IN10  PC1 ADC12_IN11
-
-    // interrupt configuration
-
-    afio.exti_cr_x(EXTIx_Px::PC, 13); // Configure EXTI13 to use PC13
-    exti.imr_set(13, true); // Enable interrupt mask register for EXTI13
-    exti.ftsr_set(13, true); // Enable rising edge trigger on EXTI13
-                             // exti.rstr_set(13, true);
-    nvic.enable_interrupt(40); // Enable EXTI15_10 interrupt
-
-    let i2c2 = I2C::new(I2C_BASE::BASE_I2C2);
-    i2c2.init();
-    rprintln!("I2C2 initialized");
-    let lcd = PCF8574_LCD::new(i2c2, PCF8574_ADDRESS);
-    lcd.lcd_initialize();
-    rprintln!("LCD initialized");
-
-    let pllrdy = rcc.read_cr_pllrdy();
-    rprintln!("PLL ready: {}", pllrdy);
-    let mut loop_count = 0;
-    // lcd.print("Hell");
-
-
-
-
-
-
+    sys_clock.store(rcc.get_sys_clock(), Ordering::Release);
+    rprintln!("System clock: {} Hz", sys_clock.load(Ordering::Acquire));
     loop {
-        let count = COUNT.load(Ordering::Relaxed).into();
-        if REFRESH_LCD.load(Ordering::Relaxed) {
-            REFRESH_LCD.store(false, Ordering::Relaxed);
-            lcd.set_cursor(0, 0);
-            lcd.clear();
-            
-            delay_sys_clk_ms(100);
-            lcd.set_cursor(0, 0);
-            delay_sys_clk_ms(100);
-            lcd.print("Hello");
-            lcd.set_cursor(0, 6);
-            lcd.print_number(loop_count);
-            delay_sys_clk_ms(100);
-            lcd.set_cursor(1, 2);
-            delay_sys_clk_ms(100);
-            lcd.print_number(count);
-            delay_sys_clk_ms(100);
-        } else {
-            loop_count += 1;
-            lcd.set_cursor(0, 6);
-            lcd.print_number(loop_count);
-            cortex_m::asm::delay(sysclk);
-            // delay_sys_clk_ms(1000);
-            // REFRESH_LCD.store(true, Ordering::Relaxed);
-        }
+        rprintln!("Hello, world!");
+        delay(sys_clock.load(Ordering::Acquire));
     }
 }
 
-#[exception]
-unsafe fn DefaultHandler(irqn: i16) {
-    rprintln!("Unhandled exception (IRQn = {})", irqn);
-    let exti = exti::new(EXTI_BASE);
-    let pr_13 = exti.pr_read(13);
-    if (irqn == 40) && pr_13 {
-        rprintln!("EXTI13 interrupt");
-        COUNT.fetch_add(1, Ordering::Relaxed);
-        REFRESH_LCD.store(true, Ordering::Relaxed);
-        exti.pr_clear(13);
-    }
-}
+// #[exception]
+// unsafe fn DefaultHandler(irqn: i16) {
+//     rprintln!("Unhandled exception (IRQn = {})", irqn);
+//     let exti = exti::new(EXTI_BASE);
+//     let pr_13 = exti.pr_read(13);
+//     if (irqn == 40) && pr_13 {
+//         rprintln!("EXTI13 interrupt");
+//         COUNT.fetch_add(1, Ordering::Relaxed);
+//         REFRESH_LCD.store(true, Ordering::Relaxed);
+//         exti.pr_clear(13);
+//     }
+// }
 
+// // fn get_sys_clock() -> u32 {
+//     // RCC 레지스터 하드코딩된 값
+//     let rcc = rcc::new();
+//     let rcc_cfgr = rcc.read_cfgr(); // 예시 값, 실제 값으로 교체 필요
 
-fn get_sys_clock() -> u32 {
-    // RCC 레지스터 하드코딩된 값
-    let rcc = rcc::new();
-    let rcc_cr = rcc.read_cr(); // 예시 값, 실제 값으로 교체 필요
-    let rcc_cfgr = rcc.read_cfgr(); // 예시 값, 실제 값으로 교체 필요
+//     // HSI 클럭 속도
+//     let hsi_clk = 8_000_000; // 8 MHz
+//     // HSE 클럭 속도 (기본값)
+//     let hse_clk = 8_000_000; // 8 MHz
 
-    // HSI 클럭 속도
-    let hsi_clk = 8_000_000; // 8 MHz
-    // HSE 클럭 속도 (기본값)
-    let hse_clk = 8_000_000; // 8 MHz
+//     // SWS 비트 확인 (시스템 클럭 소스)
+//     let sws = (rcc_cfgr >> 2) & 0b11;
 
-    // SWS 비트 확인 (시스템 클럭 소스)
-    let sws = (rcc_cfgr >> 2) & 0b11;
+//     let sysclk = match sws {
+//         0b00 => {
+//             // HSI 사용
+//             hsi_clk
+//         }
+//         0b01 => {
+//             // HSE 사용
+//             hse_clk
+//         }
+//         0b10 => {
+//             // PLL 사용
+//             // PLL 소스 확인 (PLLSRC 비트)
+//             let pllsrc = (rcc_cfgr >> 16) & 0b1;
+//             let pll_clk_in = if pllsrc == 0 {
+//                 // HSI/2
+//                 hsi_clk / 2
+//             } else {
+//                 // HSE
+//                 if (rcc_cfgr >> 17) & 0b1 == 1 {
+//                     hse_clk / 2
+//                 } else {
+//                     hse_clk
+//                 }
+//             };
 
-    let sysclk = match sws {
-        0b00 => {
-            // HSI 사용
-            hsi_clk
-        }
-        0b01 => {
-            // HSE 사용
-            hse_clk
-        }
-        0b10 => {
-            // PLL 사용
-            // PLL 소스 확인 (PLLSRC 비트)
-            let pllsrc = (rcc_cfgr >> 16) & 0b1;
-            let pll_clk_in = if pllsrc == 0 {
-                // HSI/2
-                hsi_clk / 2
-            } else {
-                // HSE
-                if (rcc_cfgr >> 17) & 0b1 == 1 {
-                    hse_clk / 2
-                } else {
-                    hse_clk
-                }
-            };
+//             // PLL 곱셈 계수 확인 (PLLMUL 비트)
+//             let pllmul = ((rcc_cfgr >> 18) & 0b1111) + 2;
+//             pll_clk_in * pllmul
+//         }
+//         _ => {
+//             // 예약된 값 (사용되지 않음)
+//             0
+//         }
+//     };
 
-            // PLL 곱셈 계수 확인 (PLLMUL 비트)
-            let pllmul = ((rcc_cfgr >> 18) & 0b1111) + 2;
-            pll_clk_in * pllmul
-        }
-        _ => {
-            // 예약된 값 (사용되지 않음)
-            0
-        }
-    };
+//     // AHB 프리스케일러 확인 (HPRE 비트)
+//     let hpre = (rcc_cfgr >> 4) & 0b1111;
+//     let ahb_prescaler = match hpre {
+//         0b0000 => 1,
+//         0b1000 => 2,
+//         0b1001 => 4,
+//         0b1010 => 8,
+//         0b1011 => 16,
+//         0b1100 => 64,
+//         0b1101 => 128,
+//         0b1110 => 256,
+//         0b1111 => 512,
+//         _ => 1,
+//     };
 
-    // AHB 프리스케일러 확인 (HPRE 비트)
-    let hpre = (rcc_cfgr >> 4) & 0b1111;
-    let ahb_prescaler = match hpre {
-        0b0000 => 1,
-        0b1000 => 2,
-        0b1001 => 4,
-        0b1010 => 8,
-        0b1011 => 16,
-        0b1100 => 64,
-        0b1101 => 128,
-        0b1110 => 256,
-        0b1111 => 512,
-        _ => 1,
-    };
-
-    sysclk / ahb_prescaler
-}
+//     sysclk / ahb_prescaler
+// }
--- src/peripherals/rcc.rs
+++ src/peripherals/rcc.rs
@@ -1,5 +1,8 @@
 #![allow(non_snake_case)]
-use crate::{peripherals::flash::{FLASH, FLASH_LATENCY}, utils::delay::delay_sys_clk_ms};
+use crate::{
+    peripherals::flash::{FLASH, FLASH_LATENCY},
+    utils::delay::delay_sys_clk_ms,
+};
 pub enum TIMxEN {
     TIM2EN = 0,
     TIM3EN = 1,
@@ -28,22 +31,21 @@ pub struct rcc {
 }
 #[allow(non_snake_case)]
 impl rcc {
-    pub fn new() -> rcc{
+    pub fn new() -> rcc {
         let base_addr = RCC_BASE;
         rcc {
-            base:       RCC_BASE,
-            cr:         (RCC_BASE + 0x00) as *mut u32,
-            cfgr:       (RCC_BASE + 0x04) as *mut u32,
-            apb2enr:    (RCC_BASE + 0x18) as *mut u32,
-            apb1enr:    (RCC_BASE + 0x1C) as *mut u32,
-            ahbenr:     (RCC_BASE + 0x14) as *mut u32,
-            bdcr:       (RCC_BASE + 0x20) as *mut u32,
-            csr:        (RCC_BASE + 0x24) as *mut u32,
+            base: RCC_BASE,
+            cr: (RCC_BASE + 0x00) as *mut u32,
+            cfgr: (RCC_BASE + 0x04) as *mut u32,
+            apb2enr: (RCC_BASE + 0x18) as *mut u32,
+            apb1enr: (RCC_BASE + 0x1C) as *mut u32,
+            ahbenr: (RCC_BASE + 0x14) as *mut u32,
+            bdcr: (RCC_BASE + 0x20) as *mut u32,
+            csr: (RCC_BASE + 0x24) as *mut u32,
         }
     }
 
-
-    /// ## CR_HSION -  HSI ON 
+    /// ## CR_HSION -  HSI ON
     /// HSI oscillator enabled
     pub fn cr_hsion(&self) {
         unsafe {
@@ -62,14 +64,10 @@ impl rcc {
     }
 
     pub fn read_cfgr(&self) -> u32 {
-        unsafe {
-            self.cfgr.read_volatile()
-        }
+        unsafe { self.cfgr.read_volatile() }
     }
     pub fn read_cr(&self) -> u32 {
-        unsafe {
-            self.cr.read_volatile()
-        }
+        unsafe { self.cr.read_volatile() }
     }
 
     pub fn set_sys_clock_32MHz(&self) {
@@ -85,11 +83,11 @@ impl rcc {
 
             let mut rcc_cfgr_val = self.cfgr.read_volatile();
 
-            // rcc_cfgr_val &= !(0b1 << 24); // Clear PLLON bit 
+            // rcc_cfgr_val &= !(0b1 << 24); // Clear PLLON bit
             rcc_cfgr_val &= !(1 << 16); // Clear PLLSRC bit (select HSI/2)
             rcc_cfgr_val &= !(0b1111 << 18); // Clear PLLMUL bits
             rcc_cfgr_val |= (0b0110 << 18); // Set PLLMUL to 8 (4 MHz * 8 = 32 MHz)
-            // rcc_cfgr_val |= (0b1110 << 18); // Set PLLMUL to 8 (4 MHz * 16 = 64 MHz)
+                                            // rcc_cfgr_val |= (0b1110 << 18); // Set PLLMUL to 8 (4 MHz * 16 = 64 MHz)
             self.cfgr.write_volatile(rcc_cfgr_val);
 
             rcc_cfgr_val = self.cfgr.read_volatile();
@@ -107,57 +105,88 @@ impl rcc {
             delay_sys_clk_ms(100);
         }
     }
+
+    /// ### CFGR_PLLMUL - PLL multiplication factor
+    /// **IMPORTANT** PLL output freq muts not exceed 72MHz
+    /// - 0000: PLL input clock x 2
+    /// - 0001: PLL input clock x 3
+    /// - 0010: PLL input clock x 4
+    /// - 0011: PLL input clock x 5
+    /// - 0100: PLL input clock x 6
+    /// - 0101: PLL input clock x 7
+    /// - 0110: PLL input clock x 8
+    /// - 0111: PLL input clock x 9
+    /// - 1000: PLL input clock x 10
+    /// - 1001: PLL input clock x 11
+    /// - 1010: PLL input clock x 12
+    /// - 1011: PLL input clock x 13
+    /// - 1100: PLL input clock x 14
+    /// - 1101: PLL input clock x 15
+    /// - 1110: PLL input clock x 16
+    /// - 1111: PLL input clock x 16
+    pub fn pllmul_set(&self, multiplication_factor: u32) {
+        if (multiplication_factor > 17) {
+            panic!("Multiplication factor must be less than 16")
+        };
+        unsafe {
+            let mut rcc_cfgr_val = self.cfgr.read_volatile();
+            rcc_cfgr_val &= !(0b1111 << 18); // Clear PLLMUL bits
+            rcc_cfgr_val |= (multiplication_factor << 18); // Set PLLMUL bits
+            self.cfgr.write_volatile(rcc_cfgr_val);
+        }
+    }
+
     
+
     pub fn set_sys_clock_64MHz(&self) {
         unsafe {
             let mut rcc_cr_val = self.cr.read_volatile();
             rcc_cr_val |= (1 << 0); // HSION
             self.cr.write_volatile(rcc_cr_val);
             while (self.cr.read_volatile() & (1 << 1)) == 0 {} // Wait until HSIRDY
-    
+
             let flash = FLASH::new(0x4002_2000);
             flash.ACR_LATENCY(FLASH_LATENCY::_2WS); // Set flash latency for 64 MHz
             flash.ACR_PRFTBE(true); // Enable prefetch buffer
-    
+
             let mut rcc_cfgr_val = self.cfgr.read_volatile();
-    
+
             // HSI/2를 PLL 소스로 선택하고 PLL 곱셈 인자를 16으로 설정
             rcc_cfgr_val &= !(1 << 16); // Clear PLLSRC bit (select HSI/2)
             rcc_cfgr_val &= !(0b1111 << 18); // Clear PLLMUL bits
             rcc_cfgr_val |= (0b1110 << 18); // Set PLLMUL to 16 (8 MHz / 2 * 16 = 64 MHz)
             self.cfgr.write_volatile(rcc_cfgr_val);
-    
+
             rcc_cr_val = self.cr.read_volatile();
             rcc_cr_val |= (1 << 24); // PLLON
             self.cr.write_volatile(rcc_cr_val);
             while (self.cr.read_volatile() & (1 << 25)) == 0 {} // Wait until PLLRDY
-    
+
             rcc_cfgr_val = self.cfgr.read_volatile();
             rcc_cfgr_val &= !(0b11 << 0); // Clear SW bits
             rcc_cfgr_val |= (0b10 << 0); // Set SW to 0b10 (PLL selected as system clock)
             self.cfgr.write_volatile(rcc_cfgr_val);
-    
+
             while (self.cfgr.read_volatile() & (0b11 << 2)) != (0b10 << 2) {} // Wait until SWS is PLL
-    
+
             delay_sys_clk_ms(100);
         }
     }
 
     /// ## CFGR_ADCPRE - ADC prescaler
-    /// #### @param **adcpre** 
+    /// #### @param **adcpre**
     /// **IMPORTANT** PLCK2 / ADCPRE > 14MHz <br/>
     /// - 00: PCLK2 divided by 2 <br/>
     /// - 01: PCLK2 divided by 4 <br/>
     /// - 10: PCLK2 divided by 6 <br/>
     /// - 11: PCLK2 divided by 8 <br/>
-    pub fn cfgr_adcpre(&self , adcpre: u32) {
+    pub fn cfgr_adcpre(&self, adcpre: u32) {
         unsafe {
             let mut rcc_cfgr_val = self.cfgr.read_volatile();
             rcc_cfgr_val &= !(0b11 << 14); // Clear ADCPRE bits
             rcc_cfgr_val |= (adcpre << 14); // Set ADCPRE bits
             self.cfgr.write_volatile(rcc_cfgr_val);
         }
-        
     }
 
     fn cr_pllon(&self) {
@@ -175,7 +204,7 @@ impl rcc {
     pub fn enable_adc1(&self) {
         unsafe {
             let mut apb2enr_val = self.apb2enr.read_volatile();
-            apb2enr_val |= (1 << 9);  // ADC1 클럭 활성화
+            apb2enr_val |= (1 << 9); // ADC1 클럭 활성화
             self.apb2enr.write_volatile(apb2enr_val);
         }
     }
@@ -260,5 +289,70 @@ impl rcc {
             }
             self.apb1enr.write_volatile(apb1enr_val);
         }
+    }
+
+    pub fn get_sys_clock(&self) -> u32 {
+        // RCC 레지스터 하드코딩된 값
+        let rcc_cfgr = self.read_cfgr(); // 예시 값, 실제 값으로 교체 필요
+
+        // HSI 클럭 속도
+        let hsi_clk = 8_000_000; // 8 MHz
+                                 // HSE 클럭 속도 (기본값)
+
+        // SWS 비트 확인 (시스템 클럭 소스)
+        let sws = (rcc_cfgr >> 2) & 0b11;
+
+        let sysclk = match sws {
+            0b00 => {
+                // HSI 사용
+                hsi_clk
+            }
+            0b01 => {
+                // HSE 사용
+                panic!("HSE not supported")
+            }
+            0b10 => {
+                // PLL 사용
+                // PLL 소스 확인 (PLLSRC 비트)
+                let pllsrc = (rcc_cfgr >> 16) & 0b1;
+                let pll_clk_in = if pllsrc == 0 {
+                    // HSI/2
+                    hsi_clk / 2
+                } else {
+                    0
+                    // HSE
+                    // if (rcc_cfgr >> 17) & 0b1 == 1 {
+                    //     hse_clk / 2
+                    // } else {
+                    //     hse_clk
+                    // }
+                };
+
+                // PLL 곱셈 계수 확인 (PLLMUL 비트)
+                let pllmul = ((rcc_cfgr >> 18) & 0b1111) + 2;
+                pll_clk_in * pllmul
+            }
+            _ => {
+                // 예약된 값 (사용되지 않음)
+                0
+            }
+        };
+
+        // AHB 프리스케일러 확인 (HPRE 비트)
+        let hpre = (rcc_cfgr >> 4) & 0b1111;
+        let ahb_prescaler = match hpre {
+            0b0000 => 1,
+            0b1000 => 2,
+            0b1001 => 4,
+            0b1010 => 8,
+            0b1011 => 16,
+            0b1100 => 64,
+            0b1101 => 128,
+            0b1110 => 256,
+            0b1111 => 512,
+            _ => 1,
+        };
+
+        sysclk / ahb_prescaler
     }
 }
